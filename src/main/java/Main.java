cd98a8bdd51c48c7e0d3f540d1de918c4a97e4a1
LeonardoTiussMoura <167460740+LeonardoTiussMoura@users.noreply.github.com>
2024-05-13 21:55:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("             -----------------          ");
+    System.out.println("             - Curso Técnico -          ");
+    System.out.println("             -----------------          ");
+    System.out.println("");
+    System.out.println("   Desde o ano passado eu me interessei em programção, códigos, etc. Eu quero muito criar meu próprio sistema operacional, espero muito que eu consiga aprender a programar para criar esse sistema.");
+    System.out.println("   O curso está sendo muito bom, as proposições e regras eu achei bem legal, fácil de aprender, gostei bastante.");
+    System.out.println("   Minha primeira vez aprendendo programação, é a profissão que quero pro meu futuro, ter um salário bom, quero evoluir para me tornar um programador de verdade.");
+    System.out.println("   Bom foi isso, quero evoluir bastante, tirar notas boas no curso e aprender ainda mais sobre programação.");
+  }
+}
